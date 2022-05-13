db861063c5b6367265a6705c3764b9f0dffe3430
Ammar050 <Ammar090@yandex.com>
2022-05-14 01:47:13 +0300

    ADD huffleCards Function

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+const cardImages = [
+    {src: '/img/helmet-1.png' }, 
+    {src: '/img/potion-1.png' }, 
+    {src: '/img/ring-1.png' }, 
+    {src: '/img/scroll-1.png' }, 
+    {src: '/img/shield-1.png' }, 
+    {src: '/img/sword-1.png' }
+]
+
+function App() {
+
+    const [ cards, setCards ] = useState([])
+    const [ turn, setTurn ] = useState(0)
+
+  // shuffle the cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages] 
+    .sort(() => Math.random() - 0.5)
+    .map(card => ({...card, id: Math.random()}))
+
+    setCards(shuffledCards)
+    setTurn(0)
+  }
+
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+    </div>
+  );
+}
+
+export default App;
